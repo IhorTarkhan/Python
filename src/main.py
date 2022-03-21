68a25a809901b16a6cf78625a60e1f5bdbaf76e7
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-03-21 12:45:58 +0200

    validate xml

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from lxml import etree
+
+parser = etree.XMLParser(dtd_validation=True)
+tree = etree.parse("sample.xml", parser)
+print(tree)
+
+
 if __name__ == "__main__":
     print("Hello World!")
