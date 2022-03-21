ce18068f1f5afab5a56f19ccec284de5e9dadf8d
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-03-21 13:21:46 +0200

    write to xml

--- src/main.py
+++ src/main.py
@@ -1,9 +1,35 @@
-from lxml import etree
+from xml.dom.minidom import Document
+from xml.dom.minidom import Element
 
-parser = etree.XMLParser(dtd_validation=True)
-tree = etree.parse("sample.xml", parser)
-print(tree)
+# Створюємо кореневий елемент
+root: Document = Document()
+xml: Element = root.createElement('map')
+root.appendChild(xml)
+# Створюємо об'єкт "країна"
+country1: Element = root.createElement("country")
+country1.setAttribute("id", "1")
+country1.setAttribute("name", "Україна")
+xml.appendChild(country1)
+# Створюємо ще один об'єкт "країна"
+country2 = root.createElement("country")
+country2.setAttribute("id", "2")
+country2.setAttribute("name", "Білорусь")
+xml.appendChild(country2)
+# Створюємо об'єкти "міста"
+city1 = root.createElement("city")
+city1.setAttribute("id", "1")
+city1.setAttribute("name", "Київ")
+city1.setAttribute("count", "3000000")
+city1.setAttribute("iscap", "1")
+xml.appendChild(city1)
+city2 = root.createElement("city")
+city2.setAttribute("id", "5")
+city2.setAttribute("name", "Вітебськ")
+city2.setAttribute("count", "350000")
+city2.setAttribute("iscap", "0")
+xml.appendChild(city2)
 
-
-if __name__ == "__main__":
-    print("Hello World!")
+xml_str = root.toxml()
+save_path_file = "countries.xml"
+with open(save_path_file, "w") as f:
+    f.write(xml_str)
