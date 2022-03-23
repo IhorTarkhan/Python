a551ee267fc89ca83a1d29163e3d1fc03080c1a0
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-03-23 17:38:10 +0200

    demo

--- src/lab1/entity/Vertex.py
+++ src/lab1/entity/Vertex.py
@@ -1,8 +1,8 @@
 class Vertex:
-    def __init__(self, _id: str, x: float, y: float):
+    def __init__(self, _id: str, x: str, y: str):
         self.id: str = _id
-        self.x: float = x
-        self.y: float = y
+        self.x: str = x
+        self.y: str = y
 
     def __repr__(self):
         return 'Vertex(id={0}, x={1}, y={2})'.format(self.id, self.x, self.y)
--- src/lab1/main.py
+++ src/lab1/main.py
@@ -1,2 +1,21 @@
+from src.lab1.entity.Polygon import Polygon
+from src.lab1.entity.Vertex import Vertex
+from src.lab1.repository.PolygonRepository import PolygonRepository
+
+polygons = None
 if __name__ == "__main__":
-    print("Hi")
+    print("All polygons:")
+    polygons = PolygonRepository.find_all_polygons()
+    print(f"length={len(polygons)}: {polygons}")
+    print()
+
+    print("Polygon with id 2:")
+    print(PolygonRepository.find_polygon_by_id("2"))
+    print()
+
+    new_polygon = Polygon("3", [Vertex("10", "4.3", "17.0")])
+    print(f"Added polygon {new_polygon}, result after:")
+    PolygonRepository.add_polygon(new_polygon)
+    polygons = PolygonRepository.find_all_polygons()
+    print(f"length={len(polygons)}: {polygons}")
+    print()
