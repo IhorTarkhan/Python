c2092632cd2932d1d251c5a9e9fe39a477e55b40
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-04-06 11:15:47 +0300

    converter

--- src/lab3/converters.py
+++ src/lab3/converters.py
@@ -35,6 +35,18 @@ def to_json_polygon_list(data: [Polygon]) -> str:
     return json.dumps(dictionary_data)
 
 
+def to_json(data) -> str:
+    if type(data) is Vertex:
+        return to_json_vertex(data)
+    if type(data) is Polygon:
+        return to_json_polygon(data)
+    if type(data) is list:
+        if type(data[0]) is Vertex:
+            return to_json_vertex_list(data)
+        if type(data[0]) is Polygon:
+            return to_json_polygon_list(data)
+
+
 def from_json_vertex(data: str) -> Vertex:
     loads: dict = json.loads(data)
     return __vertex_from_dictionary(loads)
@@ -53,3 +65,44 @@ def from_json_vertex_list(data: str) -> [Vertex]:
 def from_json_polygon_list(data: str) -> [Polygon]:
     loads: list = json.loads(data)
     return list(map(__polygon_from_dictionary, loads))
+
+
+def from_json(data: str):
+    loads = json.loads(data)
+    if type(loads) is dict:
+        try:
+            return from_json_vertex(data)
+        except KeyError:
+            pass
+        try:
+            return from_json_polygon(data)
+        except KeyError:
+            pass
+    if type(loads) is list:
+        try:
+            return from_json_vertex_list(data)
+        except KeyError:
+            pass
+        try:
+            return from_json_polygon_list(data)
+        except KeyError:
+            pass
+
+
+vertex_json_1 = to_json(Vertex("1", "2", "3"))
+polygon_json_1 = to_json(Polygon("1"))
+polygon_json_2 = to_json(Polygon("1", [Vertex("1", "2", "3"), Vertex("1", "2", "3")]))
+vertex_list_json_1 = to_json([Vertex("1", "2", "3"), Vertex("1", "2", "3")])
+polygon_list_json_1 = to_json([Polygon("1", [Vertex("1", "2", "3"), Vertex("1", "2", "3")]), Polygon("1")])
+
+print(vertex_json_1)
+print(polygon_json_1)
+print(polygon_json_2)
+print(vertex_list_json_1)
+print(polygon_list_json_1)
+print()
+print(from_json(vertex_json_1))
+print(from_json(polygon_json_1))
+print(from_json(polygon_json_2))
+print(from_json(vertex_list_json_1))
+print(from_json(polygon_list_json_1))
