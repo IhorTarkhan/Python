dc83f319b3945adac23ad07f7a74e6c5e56ff8b2
Ihor Tarkhan <ihor.tarkhan.working@gmail.com>
2022-04-10 12:42:13 +0300

    lab4

--- src/lab4/client.py
+++
@@ -1,8 +0,0 @@
-import xmlrpc.client
-
-from src.lab4.const import HOST, PORT
-
-s = xmlrpc.client.ServerProxy('http://' + HOST + ':' + PORT.__str__())
-print(s.system.listMethods())
-print(s.find_all_polygons())
-print(s.mul(2, 3))
